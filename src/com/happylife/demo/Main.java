22b4b9309c6275afc02df4d83a318f95efdb0e26
afeng <afeng@kobo.com>
2015-10-23 17:52:36 -0400

    add https server

--- src/com/happylife/demo/Main.java
+++ src/com/happylife/demo/Main.java
@@ -1,8 +1,18 @@
 package com.happylife.demo;
 
 public class Main {
+	public static int port = 9000;
 	public static void main(String[] args) {
-		SimpleHttpServer httpServer = new SimpleHttpServer();
-		httpServer.Start(7000);
+//		// start http server
+//		SimpleHttpServer httpServer = new SimpleHttpServer();
+//		httpServer.Start(port);
+		
+		// start https server
+		SimpleHttpsServer httpsServer = new SimpleHttpsServer();
+		httpsServer.Start(port);
+		
+//		System.out.println(System.getProperty("user.dir"));
+//		System.out.println(Main.class.getClassLoader().getResource("").getPath());
+		
 	}
 }
